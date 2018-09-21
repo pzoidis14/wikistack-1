8cc0f6f8537e63e12e70643a5136844eef3d7325
Chris Li <chrisli2017@gmail.com>
2018-09-21 11:38:14 -0400

    setup

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const express = require ('express')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
